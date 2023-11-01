d762e3deeab2e139217630492a115d11bea8626c
Lony <70080336+Lexsanche@users.noreply.github.com>
2023-10-31 21:21:49 -0500

    Initial commit

---
+++ personapi-dotnet/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.EntityFrameworkCore;
+using personapi_dotnet.Models.Entities;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    // Add ConflictingActionsResolver
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<PersonaDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "personapi_dotnet v1");
+    c.RoutePrefix = "swagger";
+});
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+/*
+options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "personaapi_dotnet",
+        Description = "Laboratorio por: Daniel Castellanos, Manuel Rios, Santiago Barbosa",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+}
+ */
